ae64ccbbd8212004ddb9a5fbfa4dd8307790e700
Joyce <jiz546@ucsd.edu>
2022-09-28 18:11:51 -0400

    fix for my own database

--- src/test_rest.py
+++ src/test_rest.py
@@ -22,7 +22,27 @@ def t1():
         print("\n")
 
 
+def t2():
+
+    new_url = "http://127.0.0.1:5011/api/students/jz3409"
+
+    try:
+        h_message = requests.get(new_url)
+        if h_message.status_code == 200:
+            print("\n\n Congratulations. Your end-to-end test worked. \n\n")
+            print("Application health message = \n")
+            data = h_message.json()
+            print(json.dumps(data, indent=2))
+            print("\n")
+        else:
+            print("\n\n Epic Fail. Status code = ", h_message.status_code, "\n\n")
+            print("\n")
+    except Exception as e:
+        print("\n\n Epic, Epic, Epic Fail. Exception = ", e, "\n\n")
+        print("\n")
+
+
 if __name__ == "__main__":
-    t1()
+    t2()
 
 
